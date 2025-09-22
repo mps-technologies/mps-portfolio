5bd5578ffd07a606087fe547b6d462f890062497
samuelsantos-mps <samueldamiaosantos@gmail.com>
2025-09-22 19:02:04 +0100

    nav transparente e imagem de fundo

---
+++ mps-portfolio/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        nunito: ['Nunito', 'sans-serif'],
+        bebas: ['Bebas Neue', 'cursive'],
+      },
+    },
+  },
+  plugins: [],
+}
+
