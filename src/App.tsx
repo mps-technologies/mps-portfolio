5e50fff920e5f0ce92389024c282a5fed7d15a77
samuelsantos-mps <samuel.santos.mps@gmail.com>
2026-03-21 23:00:40 +0100

    progress bar removed

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,11 @@ import { ClientWork } from './Components/ClientWork';
 import { Footer } from './Components/Footer';
 import { Hero } from './Components/Hero';
 import { Navbar } from './Components/Navbar';
-import { ScrollProgress } from './Components/ScrollProgress';
 import { Services } from './Components/Services';
 
 function App() {
   return (
     <div className="min-h-screen" style={{ backgroundColor: 'var(--color-dark)' }}>
-      <ScrollProgress />
       <Navbar />
       <main>
         <Hero />
